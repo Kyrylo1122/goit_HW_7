9e468ddb75aa349e070d55a19248d2a4b76431d6
Kyrylo1122 <93613807+Kyrylo1122@users.noreply.github.com>
2022-04-28 15:28:52 +0200

    first try

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+const imgEl = galleryItems
+  .map((e) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${e.original}">
+    <img
+      class="gallery__image"
+      src="${e.preview}"
+      data-source="${e.original}"
+      alt="${e.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+listEl.innerHTML = imgEl;
+
+listEl.addEventListener("click", onOpenModal);
+
+function onOpenModal(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" >`,
+    {
+      onShow: () => {
+        addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: () => {
+        removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  function onEscKeyPress(e) {
+    console.log(e.code);
+    if (e.code === "Escape") {
+      instance.close();
+      removeEventListener("keydown", onEscKeyPress);
+    }
+  }
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+const imgEl = galleryItems
+  .map((e) => {
+    return `<div>
+  <a class="gallery__item" href="${e.original}">
+    <img
+      class="gallery__image"
+      src="${e.preview}"
+      
+      alt="${e.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+listEl.innerHTML = imgEl;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// listEl.addEventListener("click", onOpenModal);
+
+// function onOpenModal(e) {
+//   e.preventDefault();
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const instance = basicLightbox.create(
+//     `<img src="${e.target.dataset.source}" >`,
+//     {
+//       onShow: () => {
+//         addEventListener("keydown", onEscKeyPress);
+//       },
+//       onClose: () => {
+//         removeEventListener("keydown", onEscKeyPress);
+//       },
+//     }
+//   );
+//   function onEscKeyPress(e) {
+//     console.log(e.code);
+//     if (e.code === "Escape") {
+//       instance.close();
+//       removeEventListener("keydown", onEscKeyPress);
+//     }
+//   }
+//   instance.show();
+// }
